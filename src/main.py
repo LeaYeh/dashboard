d5780430698ac8adb3c3a48d856be36add60b230
LeaYeh <smile2140@gmail.com>
2024-04-20 15:47:58 +0200

    feat: Embed the resume into streamlit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+import requests
 import streamlit as st
+import streamlit.components.v1 as components
 
 
-st.write("Hello World")
-st.balloons()
+# Custom CSS to adjust iframe borders, etc.
+st.markdown(
+    """
+    <style>
+    iframe {
+        border: 0px solid #f0f2f6;
+        border-radius: 0px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Embedding the website
+components.html(
+    f"<iframe src='https://registry.jsonresume.org/LeaYeh' width='100%' height='800'></iframe>",
+    height=800
+)
