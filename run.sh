006ee4568acfdfb1f0887714a24239cc606c1c84
LeaYeh <smile2140@gmail.com>
2024-04-20 12:28:30 +0200

    fix: Copy the nesscessary files only

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "dev" ]; then
     docker-compose -f dev-docker-compose.yaml up --build -d
 elif [ "$1" == "dashboard" ]; then
-    docker-compose -f dashboard-docker-compose.yaml up --build -d 
+    docker-compose -f dashboard-docker-compose.yaml up --build -d
 else
     echo "Invalid command. Use 'run dev' or 'run dashboard'"
 fi
